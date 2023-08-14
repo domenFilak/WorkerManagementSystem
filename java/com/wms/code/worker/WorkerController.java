423df9e075eb19f3f2e62a273af1481fc3113871
domenFilak <domen.filak2001@gmail.com>
2023-08-14 13:09:01 +0200

    Branch ButtonColorChange - changed button color in add form

--- java/com/wms/code/worker/WorkerController.java
+++ java/com/wms/code/worker/WorkerController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 public class WorkerController {
 
